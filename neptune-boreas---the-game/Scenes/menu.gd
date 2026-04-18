b6b77d5433a2bf94c7697846ff7275ad948c675d
Fon5eca <fonseblad@gmail.com>
2026-04-18 16:21:32 -0600

    Avances Deisler: Menu, musica y plantilla juego

---
+++ neptune-boreas---the-game/Scenes/menu.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Enter"):
+		get_tree().change_scene_to_file("res://Scenes/Game.tscn")
---
+++ neptune-boreas---the-game/Scenes/ship.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var speed: int = 250
+var rotate_speed: int = 5
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("Left"):
+		rotate(-rotate_speed*delta)
+	if Input.is_action_pressed("Right"):
+		rotate(rotate_speed*delta)
+	
+	if Input.is_action_pressed("Up"):
+		velocity = -transform.y*speed
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
